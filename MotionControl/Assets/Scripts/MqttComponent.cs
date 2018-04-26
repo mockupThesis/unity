f55c2780b1c1c6fd4aa770e6450a8b72d13b4705
martesz <125exc@gmail.com>
2018-04-26 17:00:45 +0200

    #3 The mqtt related code is separated

--- MotionControl/Assets/Scripts/MqttComponent.cs
+++ MotionControl/Assets/Scripts/MqttComponent.cs
@@ -1,23 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using uPLibrary.Networking.M2Mqtt.Messages;
 using UnityEngine;
 using uPLibrary.Networking.M2Mqtt;
-using System;
 using System.Text;
-using uPLibrary.Networking.M2Mqtt.Messages;
 
-public class RobotMovement : MonoBehaviour
+
+public class MqttComponent
 {
+	private IControllable controllable;
+	private string brokerHostName;
+	private MqttClient mqttclient;
 
-	public Animator animator;
-	MqttClient mqttclient;
-
-	public string brokerHostname = "mqtt.bayi.hu";
-	public int brokerPort = 1883;
-	private Quaternion received;
-
-	void Start ()
+	public MqttComponent (IControllable controllable, string brokerHostName)
 	{
+		this.controllable = controllable;
+		this.brokerHostName = brokerHostName;
+
 		Connect ();
 		Publish ("sensor/status", "Unity client connected");
 		mqttclient.MqttMsgPublishReceived += MqttMsgPublishReceived;
@@ -26,36 +24,22 @@ public class RobotMovement : MonoBehaviour
 		mqttclient.Subscribe (topics, qosLevels);
 	}
 
-	void Update ()
-	{
-	
-
-	}
-
-	void LateUpdate ()
-	{
-		Transform transform = animator.GetBoneTransform (HumanBodyBones.RightUpperArm);
-		if (Input.GetKey (KeyCode.B)) {
-			transform.eulerAngles = new Vector3 (0, 0, 90);
-		}
-		transform.eulerAngles = received.eulerAngles;
-	}
-
 	void MqttMsgPublishReceived (object sender, MqttMsgPublishEventArgs e)
 	{
 		string msg = System.Text.Encoding.UTF8.GetString (e.Message);
 		Debug.Log ("Received message from " + e.Topic + " : " + msg);
 		string[] msgParts = msg.Split ('|');
-		received = new Quaternion (float.Parse (msgParts [1]), float.Parse (msgParts [2]), float.Parse (msgParts [3]), float.Parse (msgParts [0]));
+		Quaternion received = new Quaternion (float.Parse (msgParts [1]), float.Parse (msgParts [2]), float.Parse (msgParts [3]), float.Parse (msgParts [0]));
+		controllable.control (HumanBodyBones.RightUpperArm, received);
 	}
 
 	private void Connect ()
 	{
-		mqttclient = new MqttClient (brokerHostname);
+		mqttclient = new MqttClient (brokerHostName);
 		string clientId = Guid.NewGuid ().ToString ();
 		try {
 			mqttclient.Connect (clientId);
-			Debug.Log ("MQTT client connected successfully to: " + brokerHostname);
+			Debug.Log ("MQTT client connected successfully to: " + brokerHostName);
 		} catch (Exception e) {
 			Debug.LogError ("Conn error: " + e);
 		}
@@ -65,5 +49,6 @@ public class RobotMovement : MonoBehaviour
 	{
 		mqttclient.Publish (topic, Encoding.UTF8.GetBytes (msg), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
 	}
-
 }
+
+

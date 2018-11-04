a69525c0b3896527fa26db134e7a9b22cace88f1
martesz <125exc@gmail.com>
2018-11-04 11:00:40 +0100

    MotionSensor project created
    A project created to provide simulated sensor data to the robot.

---
+++ MotionSensor/Assets/scripts/MqttComponent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using System.Text;
+
+
+public class MqttComponent
+{
+	private string brokerHostName;
+	private MqttClient mqttclient;
+
+	public MqttComponent (string brokerHostName)
+	{
+		this.brokerHostName = brokerHostName;
+
+		Connect ();
+		Publish ("sensor/status", "Unity client connected");
+		mqttclient.MqttMsgPublishReceived += MqttMsgPublishReceived;
+		string[] topics = { "sensor/RightUpperArm" };
+		byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE };
+		mqttclient.Subscribe (topics, qosLevels);
+		mqttclient.Subscribe (new string[] { "sensor/RightLowerArm" }, qosLevels);
+	}
+
+	void MqttMsgPublishReceived (object sender, MqttMsgPublishEventArgs e)
+	{
+		string msg = System.Text.Encoding.UTF8.GetString (e.Message);
+		Debug.Log ("Received message from " + e.Topic + " : " + msg);
+		string[] msgParts = msg.Split ('|');
+		Quaternion received = new Quaternion (float.Parse (msgParts [1]), float.Parse (msgParts [2]), float.Parse (msgParts [3]), float.Parse (msgParts [0]));
+
+		if (e.Topic.Equals ("sensor/RightUpperArm")) {
+			//controllable.control (HumanBodyBones.RightUpperArm, received);
+		} else {
+			//controllable.control (HumanBodyBones.RightLowerArm, received);
+		}
+	}
+
+	private void Connect ()
+	{
+		mqttclient = new MqttClient (brokerHostName);
+		string clientId = Guid.NewGuid ().ToString ();
+		try {
+			mqttclient.Connect (clientId);
+			Debug.Log ("MQTT client connected successfully to: " + brokerHostName);
+		} catch (Exception e) {
+			Debug.LogError ("Conn error: " + e);
+		}
+	}
+
+	public void Publish (string topic, string msg)
+	{
+		mqttclient.Publish (topic, Encoding.UTF8.GetBytes (msg), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
+        Debug.Log("Published message to: " + topic + " : " + msg);
+    }
+		
+}
+
+
---
+++ MotionSensor/Assets/scripts/Player.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Animator animator;
+    public string brokerHostname = "mqtt.bayi.hu";
+    public int brokerPort = 1883;
+    public MqttComponent mqtt;
+
+    private int frameCounter = 0;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        mqtt = new MqttComponent(brokerHostname);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("1"))
+        {
+            animator.Play("WAIT01", -1, 0f);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            animator.Play("WAIT02", -1, 0f);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            animator.Play("WAIT03", -1, 0f);
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            animator.Play("WAIT04", -1, 0f);
+        }
+
+        if(frameCounter++ == 10)
+        {
+            PublishRightArm();
+            PublishLeftArm();
+            frameCounter = 0;
+        }
+
+    }
+
+    public void PublishRightArm()
+    {
+        Transform rightUpperArm = animator.GetBoneTransform(HumanBodyBones.RightUpperArm);
+        Vector3 euler = rightUpperArm.eulerAngles;
+        int z = Mathf.RoundToInt(euler.z / 2);
+        mqtt.Publish("sensor/RightUpperArmZ", z.ToString());
+
+        int x = Mathf.RoundToInt(180 - euler.x / 2);
+        mqtt.Publish("sensor/RightUpperArmX", x.ToString());
+
+        Transform rightLowerArm = animator.GetBoneTransform(HumanBodyBones.RightLowerArm);
+        euler = rightLowerArm.eulerAngles;
+        z = Mathf.RoundToInt(euler.z / 2);
+        mqtt.Publish("sensor/RightLowerArmZ", z.ToString());
+    }
+
+    public void PublishLeftArm()
+    {
+        Transform leftUpperArm = animator.GetBoneTransform(HumanBodyBones.LeftUpperArm);
+        Vector3 euler = leftUpperArm.eulerAngles;
+        int z = Mathf.RoundToInt(euler.z / 2);
+        mqtt.Publish("sensor/LeftUpperArmZ", z.ToString());
+
+        int x = Mathf.RoundToInt(180 - euler.x / 2);
+        mqtt.Publish("sensor/LeftUpperArmX", x.ToString());
+
+        Transform leftLowerArm = animator.GetBoneTransform(HumanBodyBones.LeftLowerArm);
+        euler = leftLowerArm.eulerAngles;
+        z = Mathf.RoundToInt(euler.z / 2);
+        mqtt.Publish("sensor/leftLowerArmZ", z.ToString());
+    }
+
+    public string FormatMessageAsQuaternion(Vector3 euler)
+    {
+        Quaternion quaternion = Quaternion.Euler(euler);
+        string message = quaternion.w + "|" + quaternion.x + "|" + quaternion.y + "|" + quaternion.z;
+
+        return message;
+    }
+
+    public string FormatMessageAsEuler(Vector3 euler)
+    {
+        string message = euler.x + "|" + euler.y + "|" + euler.z;
+        return message;
+    }
+}
